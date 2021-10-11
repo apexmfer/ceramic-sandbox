615be01df5a1eef19fc35f5536cfc327c0b6f714
andy <andy@jonezie.com>
2021-10-11 18:52:31 -0400

    can read my own data from ceramic that i published on self.id

--- resolver.js
+++ resolver.js
@@ -3,7 +3,9 @@ import { Resolver } from 'did-resolver'
 
 
 import {CeramicClient} from '@ceramicnetwork/http-client'
+import { IDX } from '@ceramicstudio/idx'
 
+//https://developers.idx.xyz/build/installation/
 
 
 async function  start(){
@@ -16,6 +18,14 @@ async function  start(){
 const API_URL = "https://gateway.ceramic.network"
 const ceramic = new CeramicClient(API_URL)
 
+
+const aliases = {
+    alias1: 'definitionID 1',
+    alias2: 'definitionID 2',
+  }
+
+  const idx = new IDX({ ceramic, aliases })
+
 // getResolver will return an object with a key/value pair of { '3': resolver }
 // where resolver is a function used by the generic did resolver.
 const threeIdResolver = ThreeIdResolver.getResolver(ceramic)
@@ -23,9 +33,20 @@ const didResolver = new Resolver(threeIdResolver)
 
 
 
-
 const doc = await didResolver.resolve('did:3:kjzl6cwe1jw145c2q70qv1xlzljithk72u1c8s3o3duwxwtibxlmfqezyj2i5jk')
 logDocument(doc)
+
+
+//basicProfile
+
+const bucketNames = ['basicProfile','alsoKnownAs']
+
+for (let b of bucketNames ){
+    let result = await idx.get(b, 'did:3:kjzl6cwe1jw145c2q70qv1xlzljithk72u1c8s3o3duwxwtibxlmfqezyj2i5jk')
+    logDocument(result)
+}
+
+
 
 /*
 let internalDoc = doc.didDocument
