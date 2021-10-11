60a4be84cc81ca67f383ef669d9082c652605b9c
andy <andy@jonezie.com>
2021-10-11 18:37:19 -0400

    adding to resolver

--- resolver.js
+++ resolver.js
@@ -21,9 +21,31 @@ const ceramic = new CeramicClient(API_URL)
 const threeIdResolver = ThreeIdResolver.getResolver(ceramic)
 const didResolver = new Resolver(threeIdResolver)
 
-const doc = await didResolver.resolve('did:3:kjzl6cwe1jw145c2q70qv1xlzljithk72u1c8s3o3duwxwtibxlmfqezyj2i5jk')
-console.log(JSON.stringify(doc))
 
+
+
+const doc = await didResolver.resolve('did:3:kjzl6cwe1jw145c2q70qv1xlzljithk72u1c8s3o3duwxwtibxlmfqezyj2i5jk')
+logDocument(doc)
+
+/*
+let internalDoc = doc.didDocument
+
+let verificationMethods = internalDoc.verificationMethod
+
+for(let vm of verificationMethods){
+
+    let method = await didResolver.resolve(vm.id)
+    
+    logDocument(method)
+
+}*/
+
+
+
+}
+
+function logDocument(d){
+    console.log(JSON.stringify(d, null, 2 ))
 }
 
 
